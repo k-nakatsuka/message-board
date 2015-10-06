485aab2b1393627e11bcf86c84b31438c3ed7063
k_nakatsuka <hakkou218@gmail.com>
2015-10-06 05:47:08 +0000

    message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,16 @@
 class MessagesController < ApplicationController
   def index
-    @messages=Message.all
   end
-  
   def create
     @message=Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    eles
+     #メッセージを保存できなかった時
+     @message=Message.all
+      flash.now[:alert]="メッセージを保存に失敗しました"
+      render 'index'
+    end
   end
   
   private
